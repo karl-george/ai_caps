633e484d526a7a6132a1212f07a3f91b625a1b0b
Karl George <karlgeorge87@gmail.com>
2025-06-11 14:04:50 +0200

    Add redirect

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,5 @@
-import { api } from '@/convex/_generated/api';
-import { useQuery } from 'convex/react';
-import { Text, View } from 'react-native';
+import { Redirect } from 'expo-router';
 
 export default function Index() {
-  const tasks = useQuery(api.tasks.get);
-  return (
-    <View className='items-center justify-center flex-1'>
-      {tasks?.map(({ _id, text }) => <Text key={_id}>{text}</Text>)}
-    </View>
-  );
+  return <Redirect href='/login' />;
 }
