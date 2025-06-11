c55d34fcc8b04e781d93b3b4cf57d5309cd32519
Karl George <karlgeorge87@gmail.com>
2025-06-11 14:18:29 +0200

    Add checkbox

--- src/app/(app)/(public)/login.tsx
+++ src/app/(app)/(public)/login.tsx
@@ -1,10 +1,19 @@
 import { emailAtom } from '@/store/login';
+import { twFullConfig } from '@/utils/twconfig';
 import { useSignIn, useSignUp, useSSO } from '@clerk/clerk-expo';
 import Feather from '@expo/vector-icons/Feather';
+import { Checkbox } from 'expo-checkbox';
 import { Link, useRouter } from 'expo-router';
 import { useSetAtom } from 'jotai';
 import { useState } from 'react';
-import { Image, Linking, Text, TouchableOpacity, View } from 'react-native';
+import {
+  Image,
+  Linking,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 const Page = () => {
   const [loading, setLoading] = useState(false);
@@ -41,12 +50,53 @@ const Page = () => {
         </View>
 
         <View className='items-center py-8'>
-          <Image
-            source={require('@/assets/images/convex.png')}
-            className='w-40 h-40'
-          />
-          <Text className='text-center text-gray-400 font-Poppins_400Regular text-md'>
+          <View className='flex-row'>
+            <Image
+              source={require('@/assets/images/convex.png')}
+              className='w-40 h-40'
+            />
+          </View>
+          <Text className='mt-2 text-gray-400 font-Poppins_400Regular text-md'>
             AI-Powered Captions Editor
+          </Text>
+        </View>
+
+        <TextInput
+          className='p-4 my-8 text-gray-300 bg-gray-800 rounded-xl'
+          placeholder='Email'
+          placeholderTextColor={'gray'}
+          value={email}
+          onChangeText={setEmail}
+          keyboardType='email-address'
+          autoCapitalize='none'
+        />
+
+        <View className='flex-row items-center'>
+          <Checkbox
+            className='mr-4'
+            value={isTermsChecked}
+            onValueChange={setIsTermsChecked}
+            color={
+              isTermsChecked
+                ? (twFullConfig.theme.colors as any).primary
+                : undefined
+            }
+          />
+          <Text className='flex-wrap flex-1 text-gray-400 text-md font-Poppins_500Medium'>
+            I agree to the{' '}
+            <Text
+              className='text-white underline'
+              onPress={() => handleLinkPress('terms')}
+            >
+              Terms of Service
+            </Text>{' '}
+            and acknowledge Captions&apos;{' '}
+            <Text
+              className='text-white underline'
+              onPress={() => handleLinkPress('privacy')}
+            >
+              Privacy Policy
+            </Text>
           </Text>
         </View>
       </View>
