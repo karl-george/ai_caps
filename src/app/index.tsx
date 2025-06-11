d2d50c5e379b8dc2f50c3f855ca640a04f11246b
Karl George <karlgeorge87@gmail.com>
2025-06-11 13:25:30 +0200

    Add convex

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,12 @@
+import { api } from '@/convex/_generated/api';
+import { useQuery } from 'convex/react';
 import { Text, View } from 'react-native';
 
 export default function Index() {
+  const tasks = useQuery(api.tasks.get);
   return (
     <View className='items-center justify-center flex-1'>
-      <Text className='text-red-500'>Test</Text>
+      {tasks?.map(({ _id, text }) => <Text key={_id}>{text}</Text>)}
     </View>
   );
 }
