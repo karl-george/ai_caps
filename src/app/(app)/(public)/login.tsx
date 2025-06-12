ce30d8d64bf63212d699877612fafe6e77bb4648
Karl George <karlgeorge87@gmail.com>
2025-06-12 12:27:54 +0200

    Add SSO buttons

--- src/app/(app)/(public)/login.tsx
+++ src/app/(app)/(public)/login.tsx
@@ -2,11 +2,13 @@ import { emailAtom } from '@/store/login';
 import { twFullConfig } from '@/utils/twconfig';
 import { useSignIn, useSignUp, useSSO } from '@clerk/clerk-expo';
 import Feather from '@expo/vector-icons/Feather';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Checkbox } from 'expo-checkbox';
 import { Link, useRouter } from 'expo-router';
 import { useSetAtom } from 'jotai';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import {
+  ActivityIndicator,
   Image,
   Linking,
   Text,
@@ -16,7 +18,9 @@ import {
 } from 'react-native';
 
 const Page = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState<'google' | 'apple' | 'email' | false>(
+    false
+  );
   const [isTermsChecked, setIsTermsChecked] = useState(false);
   const [email, setEmail] = useState('test@email.com');
   const setEmailAtom = useSetAtom(emailAtom);
@@ -99,6 +103,61 @@ const Page = () => {
             </Text>
           </Text>
         </View>
+
+        <TouchableOpacity
+          onPress={signInWithEmail}
+          disabled={!email || !isTermsChecked || loading === 'email'}
+          className={`w-full py-4 rounded-lg mt-10 mb-14 ${
+            !email || !isTermsChecked || loading === 'email'
+              ? 'bg-gray-800'
+              : 'bg-primary'
+          }`}
+        >
+          {loading === 'email' ? (
+            <ActivityIndicator color='white' />
+          ) : (
+            <Text className='text-lg text-center text-white font-Poppins_600SemiBold'>
+              Continue
+            </Text>
+          )}
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => handleSignInWithSSO('oauth_apple')}
+          disabled={!!loading}
+          className={`w-full py-4 rounded-lg flex-row justify-center items-center bg-gray-800`}
+        >
+          {loading === 'apple' ? (
+            <ActivityIndicator color='white' />
+          ) : (
+            <>
+              <Ionicons name='logo-apple' size={28} color='white' />
+              <Text className='ml-3 text-base text-center text-white font-Poppins_600SemiBold'>
+                Continue with Apple
+              </Text>
+            </>
+          )}
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => handleSignInWithSSO('oauth_google')}
+          disabled={!!loading}
+          className={`w-full py-4 mt-4 rounded-lg flex-row justify-center items-center bg-gray-800`}
+        >
+          {loading === 'google' ? (
+            <ActivityIndicator color='white' />
+          ) : (
+            <>
+              <Image
+                source={require('@/assets/images/google.webp')}
+                className='w-6 h-6'
+              />
+              <Text className='ml-3 text-base text-center text-white font-Poppins_600SemiBold'>
+                Continue with Google
+              </Text>
+            </>
+          )}
+        </TouchableOpacity>
       </View>
     </View>
   );
