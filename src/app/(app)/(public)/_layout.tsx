878a4e7a2a601d8d1590ed8e10783aa72cfcc66c
Karl George <karlgeorge87@gmail.com>
2025-06-18 10:20:08 +0200

    Add faq screen

--- src/app/(app)/(public)/_layout.tsx
+++ src/app/(app)/(public)/_layout.tsx
@@ -5,6 +5,7 @@ const Layout = () => {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name='login' />
       <Stack.Screen name='verify' />
+      <Stack.Screen name='faq' options={{ presentation: 'modal' }} />
     </Stack>
   );
 };
