fec1214c23623f919abb19bb6b729129c8454c12
Karl George <karlgeorge87@gmail.com>
2025-06-18 14:00:00 +0200

    add screens

--- src/app/(app)/(auth)/_layout.tsx
+++ src/app/(app)/(auth)/_layout.tsx
@@ -1,8 +1,12 @@
+import { twFullConfig } from '@/utils/twconfig';
 import { useAuth } from '@clerk/clerk-expo';
-import { Stack } from 'expo-router';
-import React from 'react';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Stack, useRouter } from 'expo-router';
+import { Pressable } from 'react-native';
 
 const Layout = () => {
+  const router = useRouter();
+
   const { isSignedIn } = useAuth();
   const guard = isSignedIn ?? false;
 
@@ -12,7 +16,39 @@ const Layout = () => {
         <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
         <Stack.Screen
           name='(modal)/create'
-          options={{ presentation: 'modal' }}
+          options={{
+            presentation: 'formSheet',
+            animation: 'slide_from_bottom',
+            sheetAllowedDetents: [0.3],
+            sheetInitialDetentIndex: 0,
+            sheetGrabberVisible: false,
+            sheetCornerRadius: 20,
+            headerShown: false,
+            contentStyle: {
+              backgroundColor: (twFullConfig.theme.colors as any).dark,
+            },
+          }}
+        />
+        <Stack.Screen
+          name='(modal)/filelist'
+          options={{
+            presentation: 'fullScreenModal',
+            headerLeft: () => (
+              <Pressable onPress={() => router.dismissAll()}>
+                <Ionicons name='close' size={24} color='#fff' />
+              </Pressable>
+            ),
+            headerTitle: 'File List',
+            headerTitleStyle: {
+              color: '#fff',
+              fontSize: 20,
+              fontFamily: 'Poppins_600SemiBold',
+            },
+            headerStyle: {
+              backgroundColor: (twFullConfig.theme.colors as any).dark,
+            },
+            headerTitleAlign: 'center',
+          }}
         />
       </Stack.Protected>
     </Stack>
