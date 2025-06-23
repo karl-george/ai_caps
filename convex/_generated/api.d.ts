b4ea8a24c26deeba4a3e2fee8cf88c2793a3b024
Karl George <karlgeorge87@gmail.com>
2025-06-23 11:33:43 +0200

    Add projects schema

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,6 +15,7 @@ import type {
 } from "convex/server";
 import type * as auth from "../auth.js";
 import type * as http from "../http.js";
+import type * as projects from "../projects.js";
 import type * as users from "../users.js";
 
 /**
@@ -28,6 +29,7 @@ import type * as users from "../users.js";
 declare const fullApi: ApiFromModules<{
   auth: typeof auth;
   http: typeof http;
+  projects: typeof projects;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
--- convex/schema.ts
+++ convex/schema.ts
@@ -10,4 +10,18 @@ export const User = {
 
 export default defineSchema({
   user: defineTable(User).index('byExternalId', ['externalId']),
+  projects: defineTable({
+    userId: v.id('user'),
+    name: v.string(),
+    lastUpdate: v.number(),
+    videoSize: v.number(),
+    videoFileId: v.id('_storage'),
+    language: v.optional(v.string()),
+    status: v.union(
+      v.literal('pending'),
+      v.literal('processing'),
+      v.literal('ready'),
+      v.literal('failed')
+    ),
+  }),
 });
