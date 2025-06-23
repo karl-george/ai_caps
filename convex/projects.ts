e41e20efe93d516155247e7b4a35df1c731a82c1
Karl George <karlgeorge87@gmail.com>
2025-06-23 11:33:54 +0200

    Add create projects

---
+++ convex/projects.ts
@@ -0,0 +1,29 @@
+import { v } from 'convex/values';
+import { mutation } from './_generated/server';
+import { getUser } from './auth';
+
+export const generateUploadUrl = mutation({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.storage.generateUploadUrl();
+  },
+});
+
+export const createProject = mutation({
+  args: {
+    name: v.string(),
+    videoSize: v.number(),
+    videoFileId: v.id('_storage'),
+  },
+  handler: async (ctx, args) => {
+    const user = await getUser(ctx);
+    return await ctx.db.insert('projects', {
+      userId: user._id,
+      name: args.name,
+      lastUpdate: Date.now(),
+      videoSize: args.videoSize,
+      videoFileId: args.videoFileId,
+      status: 'pending',
+    });
+  },
+});
